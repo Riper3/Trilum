65b951d7a84550bc7bc62a6eb3bebbb899717f31
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-03-23 23:07:36 +0000

    Added logout to test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,13 @@
 @section('title', 'Inicio')
 @section('content')
 <a href="https://es.aliexpress.com/" class="btn btn-success">Test</a>
+    <a href="{{ route('logout') }}"
+       onclick="event.preventDefault();
+                     document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+    
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
 @endsection
