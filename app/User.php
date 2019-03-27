aa8ab7a61524b72da2aca9e54b1337a92f9acd87
Javier Gonzalez Llamas <lastoner4@gmail.com>
2019-03-27 20:38:13 +0000

    Changed nickname

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = ['nickname', 'email', 'password'];
+    protected $fillable = ['username', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for arrays.
